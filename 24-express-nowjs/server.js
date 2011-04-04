71a0035904c9313c70a5d7039d1681b2b22b4c8a
straps <straps@Strubuntu>
2011-04-04 13:21:23 +0200

    Testing now.js functionality

---
+++ 24-express-nowjs/server.js
@@ -0,0 +1,16 @@
+
+
+var express=require('express'), app = express.createServer();
+app.listen(8000);
+
+var everyone = require("now").initialize(app);
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.post('/message', function(req, res) {
+  everyone.now.onMsg(req.body.message);
+  res.send(200);
+});
